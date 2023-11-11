2b9153b3f61e64d37c1a0fe6623eeba20a0e43d5
Declow <ditlev.stjerne@gmail.com>
2023-11-11 21:59:00 +0100

    Cleanup

--- uiwiz/elements/textarea.py
+++ uiwiz/elements/textarea.py
@@ -14,16 +14,18 @@ class TextArea(Element):
         trigger: str = "input",
     ) -> None:
         super().__init__("textarea")
-        self.events.append(
-            {
-                "func": on_change,
-                "inputs": None,
-                "trigger": trigger,
-                "endpoint": None,
-                "target": target,
-                "swap": "swap",
-            }
-        )
         self.attributes["name"] = name
         self.attributes["placeholder"] = placeholder
         self.classes(TextArea._classes)
+
+        if on_change:
+            self.events.append(
+                {
+                    "func": on_change,
+                    "inputs": None,
+                    "trigger": trigger,
+                    "endpoint": None,
+                    "target": target,
+                    "swap": "swap",
+                }
+            )
