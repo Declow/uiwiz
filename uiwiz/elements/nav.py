3ed0cdb51f8fe729b29817c560263888a98742af
Declow <Ditlev.stjerne@gmai.com>
2023-11-14 21:13:54 +0100

    Updated nav colors

--- uiwiz/elements/nav.py
+++ uiwiz/elements/nav.py
@@ -2,7 +2,7 @@ from uiwiz.element import Element
 
 
 class Nav(Element):
-    _classes: str = "navbar bg-[#5898d4]"
+    _classes: str = "navbar bg-[#6499E9]"
 
     def __init__(self) -> None:
         super().__init__()
