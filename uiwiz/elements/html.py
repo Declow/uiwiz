ff151a35b3b4efba6cf5a74b8a6e293265d81a6a
Declow <Ditlev.stjerne@gmai.com>
2023-11-22 20:56:58 +0100

    Implemented html element to enable embedding of svg

---
+++ uiwiz/elements/html.py
@@ -0,0 +1,7 @@
+from typing import Optional
+from uiwiz.element import Element
+
+class Html(Element):
+    def __init__(self, content) -> None:
+        super().__init__()
+        self.content = content
--- uiwiz/ui.py
+++ uiwiz/ui.py
@@ -26,4 +26,5 @@ from uiwiz.elements.radio import Radio as radio
 from uiwiz.elements.range import Range as range
 from uiwiz.elements.row import Row as row
 from uiwiz.elements.spinner import Spinner as spinner
+from uiwiz.elements.html import Html as html
 from uiwiz.element import Element as element
