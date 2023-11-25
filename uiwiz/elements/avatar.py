512d6eee66c97a727dba4f051ac3476085789c1a
Declow <ditlev.stjerne@gmail.com>
2023-11-25 15:19:43 +0100

    Updated the avatar to follow the classes convention

--- uiwiz/elements/avatar.py
+++ uiwiz/elements/avatar.py
@@ -1,10 +1,13 @@
 from uiwiz.element import Element
 
 class Avatar(Element):
-    def __init__(self, path: str) -> None:
-        super().__init__("div")
-        self.classes("avatar")
+    _classes: str = "avatar"
+    _classes_inner: str = "w-{size} rounded-full"
+    
+    def __init__(self, path: str, size: int = 12) -> None:
+        super().__init__()
+        self.classes(Avatar._classes)
         with self:
-            with Element("div").classes("w-16 rounded-full"):
+            with Element("div").classes(Avatar._classes_inner.format(size=size)):
                 img = Element("img")
                 img.attributes["src"] = path
