b03a04937f19d6aa3b4d3068085201e224dab822
Declow <ditlev.stjerne@gmail.com>
2023-11-28 21:09:22 +0100

    Added burgermenu svg

--- uiwiz/svg/svg_handler.py
+++ uiwiz/svg/svg_handler.py
@@ -1,12 +1,9 @@
-
-
 from typing import Literal
-
 from anyio import Path
 
-_type = Literal["info", "error", "success", "warning", None]
+_type = Literal["info", "error", "success", "warning", "menu", None]
 
-def get_svg(svg: _type) -> str:
+def get_svg(svg: Literal["info", "error", "success", "warning", "menu"]) -> str:
     path = Path(__file__).parent / (svg + ".svg")
     with open(path, "r") as f:
         return f.read()
