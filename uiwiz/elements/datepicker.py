b30e73d25a9c5adc243044783516f4538687f554
Declow <ditlev.stjerne@gmail.com>
2024-01-09 21:33:14 +0100

    Added name to datepicker

--- uiwiz/elements/datepicker.py
+++ uiwiz/elements/datepicker.py
@@ -4,8 +4,9 @@ from uiwiz.element import Element
 
 
 class Datepicker(Element):
-    def __init__(self, default_date: Optional[datetime] = None) -> None:
+    def __init__(self, name: str, default_date: Optional[datetime] = None) -> None:
         super().__init__("input")
+        self.attributes["name"] = name
         self.attributes["type"] = "date"
         if default_date:
             self.attributes["value"] = default_date.strftime("%Y-%m-%d")
