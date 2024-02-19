dd9b695900319dbbe1d744fc2afecc28d29fa8d0
Declow <ditlev.stjerne@gmail.com>
2024-02-19 22:15:57 +0100

    Implemented a simple login response for convience

---
+++ uiwiz/login_response.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import typing
+from fastapi import Response
+
+
+class LoginResponse(Response):
+    def __init__(
+        self,
+        headers: typing.Optional[typing.Mapping[str, str]] = None,
+    ) -> None:
+        super().__init__("", 200, headers)
+        self.headers["Hx-Redirect"] = "/"
+        self.body = b""
+
+    def set_token(
+        self,
+        session_id: str,
+        session_token: str,
+        expires: typing.Union[datetime, str, int],
+    ):
+        self.set_cookie(
+            session_id,
+            session_token,
+            expires=expires,
+            secure=True,
+            samesite="strict",
+        )
