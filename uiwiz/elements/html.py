e3045ad4c6efa62680166e4ddb293b420bd63591
Declow <ditlev.stjerne@gmail.com>
2024-02-20 20:57:10 +0100

    Override the content attribute to allow raw html as a string to be set as the content

--- uiwiz/elements/html.py
+++ uiwiz/elements/html.py
@@ -4,4 +4,12 @@ from uiwiz.element import Element
 class Html(Element):
     def __init__(self, content) -> None:
         super().__init__()
-        self.content = content
+        self.__content__ = content
+
+    @property
+    def content(self) -> str:
+        return self.__content__
+
+    @content.setter
+    def content(self, content):
+        self.__content__ = content
