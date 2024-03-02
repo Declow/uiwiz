7a5ad7da85341266b7aaac99818631188749bf56
Declow <ditlev.stjerne@gmail.com>
2024-03-02 11:34:07 +0100

    Fixed css height on page

--- uiwiz/elements/drawer.py
+++ uiwiz/elements/drawer.py
@@ -40,7 +40,7 @@ class DrawerContent(Element):
 
 
 class Drawer(Element):
-    _classes: str = "drawer bg-base-100 h-screen"
+    _classes: str = "drawer bg-base-100 h-full"
 
     def __init__(self, always_open: bool = False, right: bool = False) -> None:
         super().__init__()
