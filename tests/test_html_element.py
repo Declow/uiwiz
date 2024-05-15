bcc1cd7e77b7832dce158b26f8be600d2a76d095
Declow <ditlev.stjerne@gmail.com>
2024-05-15 20:06:19 +0200

    Added more tests

---
+++ tests/test_html_element.py
@@ -0,0 +1,14 @@
+import html
+
+from uiwiz import element, ui
+
+
+def test_raw_html():
+    html = ui.html("<strong>test</strong>")
+    assert '<div id="a-0"><strong>test</strong></div>' == str(html)
+
+
+def test_escaped_html():
+    input = "<strong>test</strong>"
+    _html = ui.element(content=input)
+    assert f'<div id="a-0">{html.escape(input)}</div>' == str(_html)
