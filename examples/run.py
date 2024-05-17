1de354c492f4d1e27b21968d4effc2e6626e694d
Ditlev Stjerne <dits@tv2.dk>
2024-05-17 13:46:11 +0200

    Moved df for examples as it just generates noise

--- examples/run.py
+++ examples/run.py
@@ -1,80 +1,14 @@
 from datetime import date, datetime, timezone
 
-import pandas as pd
 import uvicorn
 from fastapi import Request
 from pydantic import BaseModel
 
 from uiwiz import UiwizApp, ui
 from uiwiz.element import Element as element
+from examples.data import df
 
 app = UiwizApp()
-
-df = pd.DataFrame(
-    [
-        {
-            "col1": "val1",
-            "col2": "val2",
-            "col3": "val1",
-            "col4": "val1asdsadasds",
-            "col5": "val1",
-            "col6": "val1",
-            "col7": "val1",
-            "col8": "val1",
-            "col9": "val1",
-            "col10": "val1",
-            "col21": "val2",
-            "col31": "val1",
-            "col41": "val1asdsadasds",
-            "col51": "val1",
-            "col61": "val1",
-            "col71": "val1",
-            "col81": "val1",
-            "col91": "val1",
-        },
-        {
-            "col1": "val1",
-            "col2": "val2",
-            "col3": "val1",
-            "col4": "val1",
-            "col5": "val1",
-            "col6": "val1",
-            "col7": "val1",
-            "col8": "val1",
-            "col9": "val1",
-            "col10": "val1",
-            "col21": "val2",
-            "col31": "val1",
-            "col41": "val1asdsadasds",
-            "col51": "val1",
-            "col61": "val1",
-            "col71": "val1",
-            "col81": "val1",
-            "col91": "val1",
-        },
-        {
-            "col1": "val1",
-            "col2": "val2",
-            "col3": "val1",
-            "col4": "val1",
-            "col5": "val1",
-            "col6": "val1",
-            "col7": "val1",
-            "col8": "val1",
-            "col9": "val1",
-            "col10": "val1",
-            "col21": "val2",
-            "col31": "val1",
-            "col41": "val1asdsadasds",
-            "col51": "val1",
-            "col61": "val1",
-            "col71": "val1",
-            "col81": "val1",
-            "col91": "val1",
-        },
-    ]
-)
-
 
 def create_nav():
     with ui.nav():
