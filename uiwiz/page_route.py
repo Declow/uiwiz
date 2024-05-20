9856cf6f1fb5b5fabc553d2573df20bea63b3da9
Declow <ditlev.stjerne@gmail.com>
2024-05-20 21:44:13 +0200

    Fixed page router

--- uiwiz/page_route.py
+++ uiwiz/page_route.py
@@ -1,9 +1,16 @@
-from typing import Callable, Optional
+from typing import Any, Callable, List, Literal, TypedDict
+
+
+class Path(TypedDict):
+    func: Callable
+    type: Literal["page", "ui"]
+    args: Any
+    kwargs: Any
 
 
 class PageRouter:
     def __init__(self) -> None:
-        self.paths = {}
+        self.paths: dict[str, Path] = {}
 
     def page(
         self,
@@ -19,7 +26,7 @@ class PageRouter:
 
     def ui(self, path: str, *args, **kwargs) -> Callable:
         def setup(func):
-            self.paths[path] = {"func": func, "type": "page", "args": args, "kwargs": kwargs}
+            self.paths[path] = {"func": func, "type": "ui", "args": args, "kwargs": kwargs}
             return func
 
         return setup
