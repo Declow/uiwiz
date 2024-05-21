cd2b6eff618632f7027e674d7c284a3ae9905f8e
Declow <ditlev.stjerne@gmail.com>
2024-05-21 20:21:07 +0200

    Added page router tests

---
+++ tests/test_page_router.py
@@ -0,0 +1,72 @@
+from fastapi.routing import APIRoute
+
+from uiwiz.app import UiwizApp
+from uiwiz.page_route import PageRouter, Path
+
+
+def test_page_router_page():
+    pr = PageRouter()
+    route = "/path"
+
+    @pr.page(route)
+    def func():
+        ...
+
+    expected_output: Path = {"args": (), "kwargs": {}, "func": func, "type": "page"}
+
+    assert expected_output == pr.paths.get(route)
+
+
+def test_page_router_ui():
+    pr = PageRouter()
+    route = "/path"
+
+    @pr.ui(route)
+    def func():
+        ...
+
+    expected_output: Path = {"args": (), "kwargs": {}, "func": func, "type": "ui"}
+
+    assert expected_output == pr.paths.get(route)
+
+
+def test_page_go_to_get_request():
+    pr = PageRouter()
+    route = "/path"
+
+    @pr.page(route)
+    def func():
+        ...
+
+    app = UiwizApp()
+    app.add_page_router(pr)
+
+    apis = {item.path: item for item in app.routes}
+
+    print(apis)
+
+    assert route == app.app_paths[func]
+    assert route == apis.get(route).path
+    assert "func" == apis.get(route).name
+    assert {"GET"} == apis.get(route).methods
+
+
+def test_ui_go_to_post_request():
+    pr = PageRouter()
+    route = "/path"
+
+    @pr.ui(route)
+    def func():
+        ...
+
+    app = UiwizApp()
+    app.add_page_router(pr)
+
+    apis = {item.path: item for item in app.routes}
+
+    print(apis)
+
+    assert route == app.app_paths[func]
+    assert route == apis.get(route).path
+    assert "func" == apis.get(route).name
+    assert {"POST"} == apis.get(route).methods
