9d8523102d17e4cbd170e16d72ea639893a6a927
Ditlev Stjerne <dits@tv2.dk>
2024-06-20 07:37:29 +0200

    Fixed tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,14 @@ from unittest import mock
 import pytest
 
 from uiwiz import UiwizApp, element
+from uiwiz.asgi_request_middelware import get_request
 
 
 @pytest.fixture(autouse=True, scope="function")
 def setup_app():
     app = UiwizApp()
 
-    with mock.patch("uiwiz.element.get_request") as api_mock:
+    with mock.patch("uiwiz.frame.get_request") as api_mock:
         mo = mock.MagicMock()
         api_mock.return_value = mo
         mo.app = app
