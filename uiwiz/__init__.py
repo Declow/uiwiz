70ffba8bcd67c7bca957f26e4952bb0b64a6d90e
Declow <ditlev.stjerne@gmail.com>
2024-10-27 21:49:29 +0100

    Added all to init

--- uiwiz/__init__.py
+++ uiwiz/__init__.py
@@ -2,3 +2,5 @@ import uiwiz.ui as ui
 from uiwiz.app import UiwizApp
 from uiwiz.element import Element
 from uiwiz.page_route import PageRouter
+
+__all__ = ["ui", "UiwizApp", "Element", "PageRouter"]
