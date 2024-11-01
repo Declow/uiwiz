2949576f4099826645d3103676a1076758303d4c
Declow <ditlev.stjerne@gmail.com>
2024-11-01 21:25:59 +0100

    Improved usage of col and row by leveraging root class variable

--- uiwiz/elements/col.py
+++ uiwiz/elements/col.py
@@ -2,6 +2,8 @@ from uiwiz.element import Element
 
 
 class Col(Element):
+    root_class: str = "flex flex-col items-start gap-4 p-4 "
+
     def __init__(self) -> None:
         super().__init__()
-        self.classes("flex flex-col items-start gap-4 p-4")
+        self.classes()
--- uiwiz/elements/row.py
+++ uiwiz/elements/row.py
@@ -2,7 +2,8 @@ from uiwiz.element import Element
 
 
 class Row(Element):
+    root_class: str = "flex flex-row flex-wrap items-start gap-4 "
+
     def __init__(self) -> None:
         super().__init__()
-
-        self.classes("flex flex-row flex-wrap items-start gap-4")
+        self.classes()
