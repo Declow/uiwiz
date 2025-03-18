a42b227e05570abe6d83fe28d293e6ce4b234973
Ditlev Stjerne <dits@tv2.dk>
2025-03-18 08:27:31 +0100

    Added submit test on modelForm

--- tests/elements/test_model_form.py
+++ tests/elements/test_model_form.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 from uiwiz import ui
+from uiwiz.app import UiwizApp
 from uiwiz.frame import Frame
 
 
@@ -21,8 +22,14 @@ def test_model_handler_instance():
     expected = """<form id="a-0" class="flex flex-col items-start gap-4 p-4  border border-base-content rounded-lg shadow-lg w-full"><div id="a-1" class="flex flex-nowrap w-full"><input id="a-3" class="input  input-bordered w-full input-md" name="name" placeholder="name" value="John" autocomplete="off"></div><div id="a-4" class="flex flex-nowrap w-full"><input id="a-6" class="input  input-bordered w-full input-md" name="age" placeholder="age" value="30" autocomplete="off"></div></form>"""
     assert expected == output
 
-# def test_model_handler_instance_submit():
-#     ui.modelForm(DataInput(name="John", age=30)).on_submit(lambda: ui.toast("Submitted"))
-#     output = Frame.get_stack().render()
-#     expected = """<form id="a-0" class="flex flex-col items-start gap-4 p-4  border border-base-content rounded-lg shadow-lg w-full" hx-target="this" hx-swap="none" hx-post="/_uiwiz/hash/278681308" hx-trigger="submit" hx-ext="json-enc"><div id="a-1" class="flex flex-nowrap w-full"><input id="a-3" class="input  input-bordered w-full input-md" name="name" placeholder="name" value="John" autocomplete="off"></div><div id="a-4" class="flex flex-nowrap w-full"><input id="a-6" class="input  input-bordered w-full input-md" name="age" placeholder="age" value="30" autocomplete="off"></div><button id="a-7" class="btn btn-md">Save</button></form>"""
-#     assert expected == output
+def test_model_handler_instance_submit():
+    app = UiwizApp()
+
+    @app.ui("/submit")
+    async def submit():
+        pass
+
+    ui.modelForm(DataInput(name="John", age=30)).on_submit(submit)
+    output = Frame.get_stack().render()
+    expected = """<form id="a-0" class="flex flex-col items-start gap-4 p-4  border border-base-content rounded-lg shadow-lg w-full" hx-target="this" hx-swap="none" hx-post="/submit" hx-trigger="submit" hx-ext="json-enc"><div id="a-1" class="flex flex-nowrap w-full"><input id="a-3" class="input  input-bordered w-full input-md" name="name" placeholder="name" value="John" autocomplete="off"></div><div id="a-4" class="flex flex-nowrap w-full"><input id="a-6" class="input  input-bordered w-full input-md" name="age" placeholder="age" value="30" autocomplete="off"></div><button id="a-7" class="btn btn-md">Save</button></form>"""
+    assert expected == output
