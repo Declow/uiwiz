f2949140e34fdb3de910c7b249c45ba40a2cd78a
Ditlev Stjerne <dits@tv2.dk>
2025-03-19 07:41:29 +0100

    Cleanup echart

--- uiwiz/elements/echart/echart.py
+++ uiwiz/elements/echart/echart.py
@@ -39,13 +39,13 @@ class EChart(Element, extensions=[LIB_PATH, JS_PATH]):
         :param height: Height of the chart container
         """
         with Element() as container:
-            self.parent_element = container
             container.classes(f"flex justify-center relative overflow-hidden items-center {height}")
             super().__init__()
-            self.attributes[EChart.name] = EChart.name
-            self.attributes[f"{EChart.name}-options"] = json.dumps(options)
-            self.attributes["hx-ext"] = EChart.name
-            self.classes("w-full h-full")
+        
+        self.attributes[EChart.name] = EChart.name
+        self.attributes[f"{EChart.name}-options"] = json.dumps(options)
+        self.attributes["hx-ext"] = EChart.name
+        self.classes("w-full h-full")
 
     def container_classes(self, input: str) -> "EChart":
         self.parent_element.classes(input)
