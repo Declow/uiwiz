d854fec08ced0d7655875e25cb668d33a4e4ed68
Declow <ditlev.stjerne@gmail.com>
2025-03-23 22:28:26 +0100

    Implemented copy button on dict

--- uiwiz/elements/dict/copy.js
+++ uiwiz/elements/dict/copy.js
@@ -1,2 +1,10 @@
-
-console.log("test")
+document.querySelectorAll(".wiz-copy-content").forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        var data = evt.currentTarget.getAttribute("data-copy-data");
+        navigator.clipboard.writeText(data).then(() => {
+            console.log("Text copied to clipboard!");
+        }).catch(err => {
+            console.error("Failed to copy:", err);
+        });
+    });
+});
