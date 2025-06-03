9e54d695f3a241ec0414f53c952022c35c2fb8fc
Ditlev Stjerne <dits@tv2.dk>
2025-06-03 07:42:59 +0200

    Added docs

--- uiwiz/elements/tabs.py
+++ uiwiz/elements/tabs.py
@@ -8,6 +8,22 @@ class Tabs(Element):
     _classes: str = "tabs-box"
 
     def __init__(self) -> None:
+        """Tabs
+        This element is used for tab navigation.
+        It should be used as a context manager to create tabs.
+
+        .. code-block:: python
+            from uiwiz import ui
+
+            with ui.tabs():
+                with ui.tab("Tab 1").active():
+                    ui.label("Content of Tab 1")
+                with ui.tab("Tab 2"):
+                    ui.label("Content of Tab 2")
+                with ui.tab("Tab 3"):
+                    ui.label("Content of Tab 3")
+
+        """
         super().__init__()
         self.classes(Tabs.root_class + Tabs._classes)
         self.attributes["role"] = "tablist"
@@ -33,6 +49,26 @@ class Tab(Element):
     _classes: str = "tab"
 
     def __init__(self, title: str, active: Optional[bool] = None) -> None:
+        """Tab
+
+        This element is used for tab navigation
+        and should be used inside a :class:`Tabs` element.
+        The first tab will be active by default if no active tab is set.
+
+        .. code-block:: python
+            from uiwiz import ui
+
+            with ui.tabs():
+                with ui.tab("Tab 1").active():
+                    ui.label("Content of Tab 1")
+                with ui.tab("Tab 2"):
+                    ui.label("Content of Tab 2")
+                with ui.tab("Tab 3"):
+                    ui.label("Content of Tab 3")
+
+        :param title: The title of the tab
+        :param active: If the tab should be active by default. Defaults to None, which will make the first tab active.
+        """
         self.selector = Element("input")
         self.selector.attributes["name"] = self.selector.parent_element.id
         self.selector.attributes["type"] = "radio"
