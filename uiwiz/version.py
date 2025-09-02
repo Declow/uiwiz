801827a60f7310fdab318af18e946e09d702b8a3
Declow <ditlev.stjerne@gmail.com>
2025-09-02 07:32:15 +0200

    Updated version

--- uiwiz/version.py
+++ uiwiz/version.py
@@ -1,10 +1,8 @@
 import importlib.metadata
 
-__version__: str = "0.0.0"  # Default version in case metadata is not available
+__version__: str = "0.0.0"
 try:
-    # Attempt to get the version from the package metadata
+    # Could fail during development
     __version__ = importlib.metadata.version("uiwiz").replace("+", ".")
 except importlib.metadata.PackageNotFoundError:
-    # If the package is not found, keep the default version
     pass
-# __version__: str = importlib.metadata.version("uiwiz").replace("+", ".")
